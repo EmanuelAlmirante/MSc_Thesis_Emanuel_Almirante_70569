d42c46ad2fefb7e316e8282ef712787f04ec0cf4
EmanuelAlmirante <emanuelalmirante@hotmail.com>
2017-02-13 23:53:20 +0000

    index.js update.

--- Server Side/public/js/index.js
+++ Server Side/public/js/index.js
@@ -17,30 +17,34 @@ $(document).ready(function()  {
         }
     });
 
+    //Mudar esta coisa.
     // To show the ID of the peer.
     peer.on('open', function() {
         $('#id').text(peer.id);
     });
 
+    //Mudar esta coisa.
     //In case of error.
     peer.on('error', function(e) {
         alert(e.message);
     })
 
+    //Mudar esta coisa.
     //Awaits for the connection. Maybe not necessary.
     peer.on('connection', function(friend) {
         console.log('Connected to ' + friend);
     });
 
     //To play the video.
-    $('video source').each(function(i, video) {
+    /*$('video source').each(function(i, video) {
         var videoData = $(video).attr('data-src');
         $(video).attr('src', videoData);
-    });
+    });*/
 
     var runned = false;
     var interval;
 
+    //Transform the name of the video in base 64, to improve delivery of files.
     function getBase64FromVideoURL(url, done) {
         var video = new Video();
 
@@ -49,14 +53,15 @@ $(document).ready(function()  {
             canvas.width = this.width;
             canvas.height = this.height;
             var ctx = canvas.getContext('2d');
-            ctx.drawImage(this, 0, 0);
-            var dataURL = canvas.toDataURL("image/png");
+            ctx.drawVideo(this, 0, 0);
+            var dataURL = canvas.toDataURL("video/mp4");
             done(dataURL);
         }
 
         video.src = url;
     };
 
+    //To do the hash of the video.
     String.prototype.hashCode = function() {
         var hash = null;
         if (this.length == 0) return hash;
@@ -64,8 +69,17 @@ $(document).ready(function()  {
         return hash;
     };
 
+    //Get the video from the server. 
     function getVideoFromServer(opts) {
         var video = document.querySelector('[data-id= "' + opts.id + '"]')
+
+        //Debug, delete.
+        var first = new Date();
+        //Debug, delete.
+        video.onload = function() {
+            var last = new Date();
+            console.log(opts.fullurl + ' : server video loading : ' + (last - first) + ' ms');
+        };
 
         video.setAttribute('src', opts.datasrc);
 
@@ -76,24 +90,14 @@ $(document).ready(function()  {
         });
     }
 
-    function loggerz(type, content, peer) {
-        var currentcount = document.getElementById(type + 'count').textContent;
-        if (currentcount.length > 0) currentcount = parseInt(currentcount);
-        currentcount++;
-        document.getElementById(type + 'count').textContent = currentcount.toString();
-
-        var currentsize = document.getElementById(type + 'size').textContent;
-        if (currentsize.length > 0) currentsize = parseInt
-
-    }
-
+    //To show the stats in 'Server:' and 'Peer:'.
     function loggerz(type, content, peer) {
         var currentcount = document.getElementById(type + '-count').textContent;
         if (currentcount.length > 0) currentcount = parseInt(currentcount);
         currentcount++;
         document.getElementById(type + '-count').textContent = currentcount.toString();
 
-        var currentsize = document.getElementById(type + '-size').textContent;
+        var currentsize = document.getElementById(type + 'size').textContent;
         if (currentsize.length > 0) currentsize = parseInt(currentsize);
         currentsize += content.length;
         document.getElementById(type + '-size').textContent = currentsize.toString();
@@ -110,26 +114,28 @@ $(document).ready(function()  {
                 var s = document.createElement('span');
                 s.setAttribute('id', peer + '-size');
                 var spacer1 = document.createElement('span');
-                spacer1.textContent = ' file(s) / '
+                spacer1.textContent = ' file(s) /';
                 var spacer2 = document.createElement('span');
-                spacer2.textContent = ' bytes'
+                spacer2.textContent = ' bytes';
                 peerlist.appendChild(el);
                 peerlist.appendChild(c);
                 peerlist.appendChild(spacer1);
                 peerlist.appendChild(s);
                 peerlist.appendChild(spacer2);
             }
-            var currentcount = document.getElementById(peer + '-count').textContent;
+
+            var currentcount = document.getElementById(type + '-count').textContent;
             if (currentcount.length > 0) currentcount = parseInt(currentcount);
             currentcount++;
-            document.getElementById(peer + '-count').textContent = currentcount.toString();
+            document.getElementById(type + '-count').textContent = currentcount.toString();
 
-            var currentsize = document.getElementById(peer + '-size').textContent;
+            var currentsize = document.getElementById(type + 'size').textContent;
             if (currentsize.length > 0) currentsize = parseInt(currentsize);
             currentsize += content.length;
-            document.getElementById(peer + '-size').textContent = currentsize.toString();
+            document.getElementById(type + '-size').textContent = currentsize.toString();
         }
     }
+
 
 
 });

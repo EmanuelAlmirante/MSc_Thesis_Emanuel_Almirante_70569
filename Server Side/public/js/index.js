0718d7870dd26989b4110af8f116e255d908107d
EmanuelAlmirante <emanuelalmirante@hotmail.com>
2017-03-13 22:20:10 +0000

    GET errors still not resolved.

--- Server Side/public/js/index.js
+++ Server Side/public/js/index.js
@@ -158,6 +158,31 @@
             stopTheirStream(outCalls);
 
         });
+
+        //PARA TESTE!
+        /*var games = new Array();
+        games[0] = "World of Warcraft";
+        games[1] = "Lord of the Rings Online";
+        games[2] = "Aion";
+        games[3] = "Eve Online";
+        games[4] = "Final Fantasy XI";
+        games[5] = "City of Heros";
+        games[6] = "Champions Online";
+        games[7] = "Dark Age of Camelot";
+        games[8] = "Warhammer Online";
+        games[9] = "Age of Conan";
+
+        //Get the list of streamers.
+        $('#streamslist').click(function() {
+
+            var list = "";
+            for (i = 0; i < games.length; i++) {
+                list += "<li>" + games[i] + "</li>";
+            }
+            $("#streamlist").append(list);
+            //FUNÇÃO PARA MOSTRAR STREAMERS!
+
+        });*/
     });
     /*End of the collaboration part of the project. */
 
